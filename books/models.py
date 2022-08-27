cdd38db5437e6cc63760e113725df4510b1b57be
Kebron <Kebronjrdaniel@gmail.com>
2022-08-27 15:40:22 +0300

    Added the admin page with custom display

--- books/models.py
+++ books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255, null=True)
+    author = models.CharField(max_length=255, null=True)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    description = models.TextField()
+    publisher = models.CharField(max_length=255)
+    published_year = models.DateTimeField()
+
+
